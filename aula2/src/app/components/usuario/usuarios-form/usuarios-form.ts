b0f45c35ad0c0dade6e7faa36baf93d69f3acb1d
andreflf <andreferreira.lima@hotmail.com>
2025-10-20 15:38:44 -0300

    feat: implenta construtor e metodo de save e findById da UsuariosForm.ts

--- aula2/src/app/components/usuario/usuarios-form/usuarios-form.ts
+++ aula2/src/app/components/usuario/usuarios-form/usuarios-form.ts
@@ -5,6 +5,7 @@ import { Usuario } from '../../../auth/usuario';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LoginService } from '../../../auth/login.service';
 import { FormsModule } from '@angular/forms';
+import { UsuarioService } from '../../../services/usuario-service';
 
 @Component({
   selector: 'app-usuarios-form',
@@ -22,71 +23,67 @@ export class UsuariosForm {
 
   rotaAtivida = inject(ActivatedRoute);
   roteador = inject(Router);
-  //usuarioService = inject(UsuarioService);
+  usuarioService = inject(UsuarioService);
   loginService = inject(LoginService);
 
-  save(){
-    
+  constructor() {
+    let id = this.rotaAtivida.snapshot.params['id'];
+    if (id) {
+      this.findById(id);
+    }
   }
 
-  // constructor() {
-  //   let id = this.rotaAtivida.snapshot.params['id'];
-  //   if (id) {
-  //     this.findById(id);
-  //   }
-  // }
+  findById(id: number) {
+    this.usuarioService.findById(id).subscribe({
+      next: (usuarioRetornado) => {
+        this.usuario = usuarioRetornado;
+        this.modoEdit = true;
+      },
+      error: (erro) => {
+        Swal.fire(erro.error, '', 'error');
+      },
+    });
+  }
 
-  // findById(id: number) {
-  //   this.usuarioService.findById(id).subscribe({
-  //     next: (usuarioRetornado) => {
-  //       this.usuario = usuarioRetornado;
-  //       this.modoEdit = true;
-  //     },
-  //     error: (erro) => {
-  //       Swal.fire(erro.error, '', 'error');
-  //     },
-  //   });
-  // }
+  save() {
+    if (this.usuario.id > 0) {
+      // UPDATE
+      this.usuarioService.update(this.usuario, this.usuario.id).subscribe({
+        next: (mensagem) => {
+          Swal.fire(mensagem, '', 'success');
+          this.roteador.navigate(['admin/usuarios']);
+        },
+        error: (erro) => {
+          Swal.fire(erro.error, '', 'error');
+        },
+      });
+    } else {
+      if (this.senha != this.senhaNovamente) {
+        Swal.fire('As senhas digitadas não conferem!', '', 'error');
+        return;
+      }
 
-  // save() {
-  //   if (this.usuario.id > 0) {
-  //     // UPDATE
-  //     this.usuarioService.update(this.usuario, this.usuario.id).subscribe({
-  //       next: (mensagem) => {
-  //         Swal.fire(mensagem, '', 'success');
-  //         this.roteador.navigate(['admin/usuarios']);
-  //       },
-  //       error: (erro) => {
-  //         Swal.fire(erro.error, '', 'error');
-  //       },
-  //     });
-  //   } else {
-  //     if (this.senha != this.senhaNovamente) {
-  //       Swal.fire('As senhas digitadas não conferem!', '', 'error');
-  //       return;
-  //     }
+      if (this.senha.length < 6) {
+        Swal.fire('A senha precisa ter no mínimo 6 caracteres!', '', 'error');
+        return;
+      }
 
-  //     if (this.senha.length < 6) {
-  //       Swal.fire('A senha precisa ter no mínimo 6 caracteres!', '', 'error');
-  //       return;
-  //     }
-
-  //     this.usuario.password = this.senha;
-  //     this.usuario.role = 'USER';
-  //     // SAVE
-  //     this.usuarioService.save(this.usuario).subscribe({
-  //       next: (mensagem) => {
-  //         Swal.fire(mensagem, '', 'success');
-  //         if (this.loginService.getToken() == null)
-  //           this.roteador.navigate(['login']);
-  //         else  if (this.loginService.getToken() == '')
-  //           this.roteador.navigate(['login']);
-  //         else this.roteador.navigate(['admin/usuarios']);
-  //       },
-  //       error: (erro) => {
-  //         Swal.fire(erro.error, '', 'error');
-  //       },
-  //     });
-  //   }
-  // }
+      this.usuario.password = this.senha;
+      this.usuario.role = 'USER';
+      // SAVE
+      this.usuarioService.save(this.usuario).subscribe({
+        next: (mensagem) => {
+          Swal.fire(mensagem, '', 'success');
+          if (this.loginService.getToken() == null)
+            this.roteador.navigate(['login']);
+          else  if (this.loginService.getToken() == '')
+            this.roteador.navigate(['login']);
+          else this.roteador.navigate(['admin/usuarios']);
+        },
+        error: (erro) => {
+          Swal.fire(erro.error, '', 'error');
+        },
+      });
+    }
+  }
 }
