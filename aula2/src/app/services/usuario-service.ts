11d2dd6c68a3ec2ba9b9117282e27706b89db25c
andreflf <andreferreira.lima@hotmail.com>
2025-10-20 15:34:14 -0300

    feat: cria e implementa UsuarioService.ts

---
+++ aula2/src/app/services/usuario-service.ts
@@ -0,0 +1,50 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+import { Usuario } from '../auth/usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  http = inject(HttpClient);
+  API = environment.SERVIDOR + "/api/usuario";
+
+  constructor(){}
+
+  findAll(): Observable<Usuario[]>{
+    return this.http.get<Usuario[]>(this.API+'/findAll');
+  }
+
+  findById(id: number): Observable<Usuario>{
+    return this.http.get<Usuario>(this.API+'/findById/'+id);
+  }
+
+  deleteById(id: number): Observable<string>{
+    return this.http.delete<string>(this.API+'/deleteById/'+id, {responseType: 'text' as 'json'});
+  }
+
+  save(usuario: Usuario): Observable<string> {
+    return this.http.post<string>(this.API+'/save', usuario, {responseType: 'text' as 'json'});
+  }
+
+  update(usuario: Usuario, id: number): Observable<string> {
+    return this.http.put<string>(this.API+'/update/'+id, usuario, {responseType: 'text' as 'json'});
+  }
+
+  alterarSenha(novaSenha: string, id: number): Observable<string> {
+    let par = new HttpParams().set('novaSenha',novaSenha); //tem que ser o mesmo nome da variável definida na controller do backend
+    return this.http.put<string>(this.API+'/alterarSenha/'+id, null, {params: par, responseType: 'text' as 'json'});
+  }
+
+//implementar!
+  //   findByNome(nome: string): Observable<Usuario[]>{
+  //   let par = new HttpParams()
+  //   .set('nome',nome);
+    
+  //   return this.http.get<Usuario[]>(this.API+'/findByNome', {params: par});
+  // }
+  
+}
